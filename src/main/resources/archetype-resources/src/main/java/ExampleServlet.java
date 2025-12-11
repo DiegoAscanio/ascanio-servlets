e85c6fc967e2928b6dc33e96cebe1fcd197c42ba
DiegoAscanio <dascaniosantos@gmail.com>
2025-12-11 19:23:00 -0300

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/ExampleServlet.java
@@ -0,0 +1,23 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ExampleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.getWriter().println("<!DOCTYPE html>");
+        resp.getWriter().println("<html><head><title>Example Servlet</title></head>");
+        resp.getWriter().println("<body><h1>Example Servlet (Without JSP)</h1></body></html>");
+    }
+}
